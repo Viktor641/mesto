99d8560a9881b7dfeca5a2c083cdd4a2ea4b060c
Viktor Sudarkin <victor.sudarckin@yandex.ru>
2023-01-30 20:25:28 +0400

    feat: сделать адаптив от 320px до 1280px

---
+++ index.js
@@ -0,0 +1,29 @@
+const aboutButton = document.querySelector(".navigation__button_type_about");
+
+console.log('моя кнопка: ', aboutButton)
+
+const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__close");
+
+const toggleOpenPopup = () => {
+  popup.classList.toggle("popup_opened");
+};
+
+const handleAboutButtonClick = () => {
+  toggleOpenPopup();
+};
+
+const handleCloseButtonClick = () => {
+  toggleOpenPopup();
+};
+
+const handleOverlyClick = (event) => {
+  if (event.target === event.currentTarget) {
+    toggleOpenPopup();
+  }
+};
+
+aboutButton.addEventListener("click", handleAboutButtonClick);
+closeButton.addEventListener("click", handleCloseButtonClick);
+
+popup.addEventListener("click", handleOverlyClick);
